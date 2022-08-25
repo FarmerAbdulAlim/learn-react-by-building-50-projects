8043f52edc20d4b6f86494b50155f8df5c024a52
travelerabdulalim <travelerabdulalim@gmail.com>
2022-08-25 09:43:00 +0600

    project 4

--- 04-simple-quotes-generator/src/App.js
+++ 04-simple-quotes-generator/src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
+import Quote from './components/Quote';
 
 function App() {
   return (
@@ -8,3 +8,5 @@ function App() {
     </div>
   );
 }
+
+export default App;
--- 04-simple-quotes-generator/src/components/Quote.js
+++ 04-simple-quotes-generator/src/components/Quote.js
@@ -1,34 +1,26 @@
 import React from "react";
+import {quotes} from "../data/quotes";
 
-class Quote {
+const qt = quotes();
+
+
+export default class Quote extends React.Component {
+    state = {randomQuoteIndex: 0};
+    handleChange = () => {
+        this.setState({
+            randomQuoteIndex: Math.round(Math.random()*30)
+        })
+    }
     render() {
+        const idx = this.state.randomQuoteIndex;
         return(
-            
+            <div>
+                <div className="quote-section">
+                    <h2>{qt[idx].quote}</h2>
+                    <h3>---{qt[idx].author}</h3>
+                </div>
+                <button onClick={this.handleChange}>Generate Random Quote</button>
+            </div>
         )
     }
 }
-
-
-
-// const Quote = () => {
-//     const [qt, setQuote] = useState([]);
-//     useEffect(() => {
-//       const x = Math.floor(Math.random()*29);
-//       const url = "https://dummyjson.com/quotes/"+x;
-//       fetch(url)
-//       .then(response => response.json())
-//       .then(data => setQuote(data))
-//     }, [])
-//     const likeClicked = () => console.log(qt);
-//     return (
-//       <div>
-//         <h1>Quotes That Can Change Your Life</h1>
-//         <h2>{qt.quote}</h2>
-//         <h3>---{qt.author}</h3>
-//         <button onClick = {likeClicked}>Like</button>
-//         <button>Dislike</button>
-//       </div>
-//     )
-//   }
-  
-//   export default App;
